ccdd43ab10ec890127f3dbf27a9121d167685785
Lucie Svobodová <xsvobo1x@stud.fit.vutbr.cz>
2024-04-29 10:45:05 +0200

    PatchGAN 70x70 original used

--- discriminator.py
+++ discriminator.py
@@ -1,28 +1,45 @@
 import torch
 import torch.nn as nn
-import torchvision.models as models
 
 KERNEL_SIZE = 4
 PADDING = 1
 
-class Discriminator(nn.Module):
-    def __init__(self):
+class Block(nn.Module):
+    def __init__(self, in_channels, out_channels, stride):
         super().__init__()
-        # Load pre-trained VGG model (e.g., VGG16)
-        self.model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
-        # Choose which layers to use for feature extraction
-        self.layers = nn.Sequential(*list(self.model.children())[:5])  # Example: Use first 4 layers of VGG16
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=KERNEL_SIZE, stride=stride, padding=PADDING, bias=True, padding_mode='reflect'),
+            nn.InstanceNorm2d(out_channels),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
     
     def forward(self, x):
-        # Forward pass through the chosen layers of the pre-trained VGG model
-        features = self.layers(x)
-        # print(features)
-        return features
+        return self.conv(x)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=3, features=[64, 128, 256, 512]):
+        super().__init__()
+        self.initial = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels=features[0], kernel_size=KERNEL_SIZE, stride=2, padding=1, padding_mode='reflect'),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        layers = []
+        in_channels = features[0]
+        for feature in features[1:]:
+            layers.append(Block(in_channels, out_channels=feature, stride=1 if feature == features[-1] else 2))
+            in_channels = feature
+        layers.append(nn.Conv2d(in_channels, out_channels=1, kernel_size=KERNEL_SIZE, stride=1, padding=1, padding_mode='reflect'))
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.initial(x)
+        return torch.sigmoid(self.model(x))
     
 def test():
     x = torch.randn((1,3,256,256))
-    # model = Discriminator(in_channels=3)
-    model = Discriminator()
+    model = Discriminator(in_channels=3)
     preds = model(x)
     print(model)
     print(preds.shape)
